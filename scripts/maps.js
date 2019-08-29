df24a831d3a0e72ea9e2dc0baed963bb48d84fc0
Julian <julianofhernandez@gmail.com>
2019-08-28 19:30:58 -0800

    Deployed c9a8cdc with MkDocs version: 1.0.4

---
+++ scripts/maps.js
@@ -0,0 +1,15 @@
+// This file is added to each page
+
+// Points at the map with the ID of vanHuzen
+var vanHuzen = L.map('vanHuzen').setView([40.6, -124], 10);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox.streets'
+}).addTo(vanHuzen);
+
+var marker = L.marker([40.4889985,-123.9689541]).addTo(vanHuzen);
+marker.bindPopup("<b>Van Huzen</b>").openPopup();
